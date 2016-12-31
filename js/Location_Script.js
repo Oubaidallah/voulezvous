449ebb87df0b7cde26203a9d20c295d4f8f1f16a
oubaidallah <ayadioubaidallah@gmail.com>
2016-12-31 02:09:14 +0100

    After migration 0.1

--- js/Location_Script.js
+++ js/Location_Script.js
@@ -1,15 +1,31 @@
 
         if (navigator.geolocation) {
             // L'API est disponible
-            alert("API mawjouda");
         } else {
             // Pas de support, proposer une alternative ?
-            alert("API mehich mawjouda");
         }
 
         if (location != null) {
-            alert("activer le gps!");     
+            var currentUserLS = localStorage.getItem('currentUserId');
+            var getLatitude = localStorage.getItem('currentLatitude');
+            var getLongitude = localStorage.getItem('currentLongitude');
+
+            var userDataToSend = { "latitude": getLatitude, "longitude": getLongitude };
+            //alert(JSON.stringify(userDataToSend));
+            $.ajax({
+                url: "http://voulezvous.io/api/updateProfile/" + 28,
+                type: "POST",
+                data: JSON.stringify(userDataToSend),
+                contentType: "application/json",
+                success: function (results) {
+                    //alert("ok" + results);
+                },
+                error: function (data) {
+                    //alert("erreur" + data)
+                },
+            });
+            //alert("gps déja activer!");     
         }
         else {
-            alert("gps déja activer!");
+            //alert("gactiver le gps!");
         }
